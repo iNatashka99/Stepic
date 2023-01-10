bca82d89e461f71d1d54026465fda0c8000f258c
iNatashka99 <iNatashka99@yandex.ru>
2023-01-10 18:15:02 +0300

    generics

---
+++ src/javabasiccourse/generics/Pair.java
@@ -0,0 +1,12 @@
+package javabasiccourse.generics;
+
+/*
+Реализуйте generic-класс Pair, похожий на Optional, но содержащий пару элементов разных типов и не
+запрещающий элементам принимать значение null.
+Реализуйте методы getFirst(), getSecond(), equals() и hashCode(), а также статический фабричный метод
+of(). Конструктор должен быть закрытым (private).
+*/
+public class Pair {
+
+}
+
